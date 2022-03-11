fc5f9d884c1e6d79666d65838d20b8891f2cb754
Ogawa Daisuke <99077817+ogawa-daisuke514@users.noreply.github.com>
2022-03-11 12:04:08 +0000

    [Add] server許可

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "30ddeb4f2d9a4230b7b9b5c059234761.vfs.cloud9.us-east-1.amazonaws.com"
 end
